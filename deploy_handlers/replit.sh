fbc72a9c2f2e9359c5b4bddbfa71772768d3fe24
Alex <gilplay3000@gmail.com>
2023-04-20 08:09:47 -0300

    Adicionar arquivos para evitar erros na repl.it

---
+++ deploy_handlers/replit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rm -f poetry.lock && rm -f pyproject.toml && rm -rf .upm
+pip3 uninstall poetry -y
+
+if [ ! -d "venv" ] || [ ! -f "./venv/bin/requirements.txt" ] || [ ! "$REPL_SLUG-$REPL_OWNER" == "$(cat ./venv/.deployed)" ]; then
+  rm -rf venv && rm -rf .config && rm -rf .cache
+  rm -f poetry.lock && rm -f pyproject.toml
+  echo "##################################"
+  echo "## Inicializando virtual_env... ##"
+  echo "##################################"
+  python3 -m venv venv
+  . venv/bin/activate
+  python3 -m pip install -U pip
+  echo "#################################################"
+  echo "## Instalando dependências...                  ##"
+  echo "## (Esse processo pode demorar até 3 minutos). ##"
+  echo "#################################################"
+  pip3 install -U -r requirements.txt --no-cache-dir
+  cp -r requirements.txt ./venv/bin/requirements.txt
+  echo -n "$REPL_SLUG-$REPL_OWNER" > ./venv/.deployed
+
+elif ! cmp --silent -- "./requirements.txt" "./venv/bin/requirements.txt"; then
+  echo "############################################"
+  echo "## Instalando/Atualizando dependências... ##"
+  echo "############################################"
+  . venv/bin/activate
+  pip3 install -U -r requirements.txt --no-cache-dir
+  cp -r requirements.txt ./venv/bin/requirements.txt
+
+else
+  . venv/bin/activate
+
+fi
+
+python3 main.py
